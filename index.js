f56ac11f83400c7f0074f043e73fddb95782d5fd
Danipaz1 <dn.velasquezdevia@gmail.com>
2021-05-26 09:53:15 -0700

    modulo.js actualizado

--- index.js
+++ index.js
@@ -3,19 +3,8 @@
 };*/
 
 const mdLinks = require('./modulo.js');
-let anyFile = process.argv[2];
+module.exports = mdLinks;
 
-"use strict";
 
-let fs = require('fs');
-let markdownLinkExtractor = require('markdown-link-extractor');
-
-let markdown = fs.readFileSync('README.md').toString();
-
-let links = markdownLinkExtractor(markdown);
-
-links.forEach(function (link) {
-    console.log(link);
-});
 
 
--- modulo.js
+++ modulo.js
@@ -1,20 +1,111 @@
   
 let fs = require('fs');
 let fetch = require('node-fetch');
-let path = require('path');
-let anyFile = process.argv[2];
-let pathFile = path.resolve(anyFile);
+let pathFile = require('path');
+let markdownLinkExtractor = require('markdown-link-extractor');
 let chalk = require('chalk');
 
-let mdLinks = (ruta, options = {validate:false}) => {
+/*let mdLinks = (pathFile, options = {validate:false}) => {
 
-    let mdLinkExtractor = require('markdown-link-extractor');
-    let extName = path.extname(pathFile);
+    let markdownLinkExtractor = require('markdown-link-extractor');
     let stats = fs.statSync(pathFile);
+    let extName = path.extname(pathFile);
+    
 
     if(extName === '.md'){
-        let markdown = fs.readFile(pathFile).toString();
-        let links = markdownLinkExtractor(markdown);
+        let markdown = fs.readFileSync(pathFile).toString();
+        let links = markdowndLinkExtractor(markdown);
         let arrayFetch = [];
-        console.log(links);
+        //recorrer links
+        links.forEach(function (link) {
+            console.log(link);
+        })
 }} 
+
+mdLinks(pathFile).then(console.log);*/
+
+//module.export = mdLinks;
+
+let mdLinks = (path, options) => {
+
+let abPath = pathFile.resolve(path);
+let extName = pathFile.extname(path);
+let stats = fs.statSync(path);
+let throughDir = stats.isDirectory();
+
+
+if(extName === '.md'){
+let markdown = fs.readFileSync(abPath).toString();
+let links = markdownLinkExtractor(markdown);
+
+    const linkCallback = links.map ((link) => {
+    return new Promise((resolve, reject) => {
+
+        
+        fetch(link.href)
+        .then((response) => {
+
+            if (response.status === 200) {
+                resolve({
+                    "Link": link.href,
+                    "Validación": "Ok",
+                    "Ruta": abPath,
+                    "Texto": link.text,
+                    
+                })
+            }
+            else {
+                resolve({
+                    "Link": link.href,
+                    "Validación": "Link not found",
+                    "Ruta": abPath,
+                    "Texto": link.text,
+                })
+            }
+        })
+        .catch(err => {
+            resolve({
+                "Link": link,
+                "Validación": 'Link not found',
+                "Ruta": abPath,
+                "Texto": link.text,
+            })
+        })
+});
+});
+return Promise.all(linkCallback)
+
+
+}
+
+else if (throughDir === true) {
+    const readDir = fs.readdirSync(path)
+    return (Promise.all(readDir.map((anyFile) => {
+        const files = pathFile.join(abPath, anyFile);
+        //agreagar option
+        return mdLink(files);
+    })).then(abArray => {
+        let emptyArray = []
+
+        for (let i= 0 ; i < abArray.length; i++){
+            emptyArray = emptyArray.concat(abArray[i])
+            }
+
+        return emptyArray;
+    })
+
+
+    )
+
+} else {
+    return Promise.resolve([]);
+
+
+}
+
+}
+
+if (require.index === module)
+mdLink(process.argv[2]).then(console.log);
+
+module.exports = mdLinks;
