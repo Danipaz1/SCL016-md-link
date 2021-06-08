83c74f8c2307fb293fd6bb41921b0188b0a16eeb
Danipaz1 <dn.velasquezdevia@gmail.com>
2021-06-07 20:02:45 -0700

    validate con status

--- modulo.js
+++ modulo.js
@@ -1,9 +1,7 @@
 let fs = require('fs');
 let fetch = require('node-fetch');
 let path = require('path');
-let chalk = require('chalk');
 let markdowndLinkExtractor = require('markdown-link-extractor');
-
 
 //RUTA ABS
 const anyFile = process.argv[2];
@@ -13,15 +11,7 @@ let absPath = path.resolve(anyFile);
 let mdFile = path.extname(absPath);
 
 //option --validate
-let option = process.argv[3];
-
-
-
-
-
-//const mdLinks = (ruta, options = {validate:false}) => {
-//}
-
+let options = process.argv[3];
 
 //Leer un archivo con ruta absoluta
 
@@ -32,54 +22,48 @@ const readAFile = (anyFile, options) => {
 
 
         fs.readFile(anyFile, 'utf8', (err, document) => {
+            let links = markdowndLinkExtractor(document);
+            let absPath = path.resolve(anyFile);
+            let arrayLink = links.map((link) => {
+                return ({ absPath, link });
 
-            if (mdFile === '.md') {
-                //let pathLink = [].concat(absPath, links);
-                //entries= Object.entries(pathLink);
-                let links = markdowndLinkExtractor(document);
-                let absPath = path.resolve(anyFile);
-                let arrayLink = links.map((link) => {
-                    return ({ absPath, link });
+            })
+
+            if (options === '--validate') {
+
+                const arrayFetch = arrayLink.map((linkItem) => {
+                    return fetch(linkItem.link)
+                })
+
+                Promise.allSettled(arrayFetch).then(res => {
+                    res.forEach((resFetch, index) => {
+                        //console.log(resFetch.status)
+                        if (resFetch.status == 'rejected') {
+                            //console.log(resFetch.reason.errno)
+                            arrayLink[index].status = resFetch.reason.errno;
+
+                        }
+                        if (resFetch.status == 'fulfilled') {
+                            //sconsole.log(resFetch.value.status)
+                            arrayLink[index].status = resFetch.value.status
+
+                        }
+                    })
+
+                    resolve(arrayLink)
+
 
                 })
-                    //resolve (arrayLink);
+                    .catch(err => reject(err))
 
-                    
+            } else {
 
-                if (options === '--validate') {
+                if (mdFile === '.md') {
 
-                    const arrayFetch = arrayLink.map((linkItem) => {
-                        return fetch(linkItem.link)
-                    })
-
-                    Promise.allSettled(arrayFetch).then(res => {
-                        res.forEach((resFetch, index) => {
-                            //console.log(resFetch.status)
-                            if (resFetch.status == 'rejected') {
-                                //console.log(resFetch.reason.errno)
-                                arrayLink[index].status = resFetch.reason.errno;
-
-                            }
-                            if (resFetch.status == 'fulfilled') {
-                                //sconsole.log(resFetch.value.status)
-                                arrayLink[index].status = resFetch.value.status
-
-                            }
-
-                            return arrayLink;
-                        })
-
-                        resolve(arrayLink)
-
-
-                    })
-                        .catch(err => reject(err))
+                    resolve(arrayLink);
 
                 }
-
             }
-
-
 
         })
 
